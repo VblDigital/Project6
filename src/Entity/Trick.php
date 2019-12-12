f97d72cf7399d8b4ee54d5ed99401ff342b7cd94
vbopenclass <vbopenclass@gmail.com>
2019-12-12 09:54:03 +0100

    Trick(Unique)Added validation that name is unique

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -5,9 +5,15 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ * @UniqueEntity(
+ *     fields={"name"},
+ *     message="Ce nom de Trick est déjà utilisé."
+ * )
  */
 class Trick
 {
